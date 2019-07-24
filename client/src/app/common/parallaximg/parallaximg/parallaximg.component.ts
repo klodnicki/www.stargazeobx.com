a1c1d6294e7838d679ed1b362d96fccddb299e1b
Zack Klodnicki <klodzack@udel.edu>
2019-07-24 12:23:35 -0400

    More progress

--- client/src/app/common/parallaximg/parallaximg/parallaximg.component.ts
+++ client/src/app/common/parallaximg/parallaximg/parallaximg.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, HostBinding, OnChanges } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-parallaximg',
   templateUrl: './parallaximg.component.html',
   styleUrls: ['./parallaximg.component.scss']
 })
-export class ParallaximgComponent implements OnInit {
+export class ParallaximgComponent implements OnChanges {
   @Input() src: string;
+  @HostBinding('style.background-image') private image: SafeStyle;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    this.image = this.sanitizer.bypassSecurityTrustStyle(this.src);
   }
 
 }
