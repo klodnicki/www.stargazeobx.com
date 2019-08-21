ebc0b372ccbeb4b639e4c84e0a1e1a3c772806e7
Zack Klodnicki <klodzack@udel.edu>
2019-08-21 09:29:56 -0400

    Fixed parallax math

--- client/src/app/common/block/block.component.ts
+++ client/src/app/common/block/block.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, ViewChild, HostListener, OnChanges, SimpleCha
 })
 export class BlockComponent implements OnInit, OnChanges {
   @Input() background: string;
-  @Input() parallaxDistance = 0;
+  @Input() parallaxDistance = 1;
   @Input() shrink = false;
   @ViewChild('bg') private bg;
   @ViewChild('fg') private fg;
@@ -19,32 +19,34 @@ export class BlockComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes.parallaxDistance) { this.parallaxScroll(); }
+    if (changes.parallaxDistance) {
+      if (changes.parallaxDistance.currentValue === -1) { this.parallaxDistance = Infinity; }
+      this.parallaxScroll();
+    }
   }
 
   @HostListener('window:scroll')
   @HostListener('window:wheel')
   @HostListener('window:resize')
   private parallaxScroll() {
-    if (this.parallaxDistance === 0) { return; }
+    if (this.parallaxDistance === 1) { return; }
     if (!this.bg || !this.bg.nativeElement) { return; }
     if (!this.fg || !this.fg.nativeElement) { return; }
 
     const viewportHeight = document.documentElement.clientHeight;
     const thisRect = this.fg.nativeElement.getBoundingClientRect();
 
-    const start = viewportHeight;
-    const end = -thisRect.height;
-    const now = thisRect.y;
+    const scrollStart = 0;
+    const scrollEnd = viewportHeight - thisRect.height;
+    const scrollNow = thisRect.y;
 
-    const percent = (now - start) / (end - start);
+    const percent = (scrollNow - scrollStart) / (scrollEnd - scrollStart);
 
-    const move = percent * this.parallaxDistance * -1;
+    const move = (thisRect.y * -1) + (scrollEnd - scrollStart) * percent / this.parallaxDistance;
 
-    console.log(percent);
-
-    this.bg.nativeElement.style.height = Number(100 + Math.abs(this.parallaxDistance)).toString() + '%';
-    this.bg.nativeElement.style.top = move.toString() + '%';
+    this.bg.nativeElement.style.height =
+      Number(thisRect.height + scrollEnd - (scrollEnd - scrollStart) / this.parallaxDistance).toString() + 'px';
+    this.bg.nativeElement.style.top = move.toString() + 'px';
   }
 
 }
