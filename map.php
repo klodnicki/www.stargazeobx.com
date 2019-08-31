d34844f4b078349f44466569688dec345c262b4e
Zack Klodnicki <klodzack@udel.edu>
2019-08-30 22:50:18 -0400

    Fix embedded map (hopefully)

--- map.php
+++ map.php
@@ -9,7 +9,8 @@
 <center><div id="container" align="center">
 	<?php include('PHPDocs/menu.php'); ?>
 	
-<p /><center><iframe width="95%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=54195 Osprey Way++Frisco+NC+27936 (OBX Star Gaze)&amp;num=1&amp;t=h&amp;vpsrc=0&amp;sll=35.227041,-75.638466&amp;sspn=0.019877,0.042272&amp;hl=en&amp;ie=UTF8&amp;ll=35.226805,-75.63897&amp;spn=0.005013,0.010568&amp;z=14&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?q=54195 Osprey Way++Frisco+NC+27936 (OBX Star Gaze)&amp;num=1&amp;t=h&amp;vpsrc=0&amp;sll=35.227041,-75.638466&amp;sspn=0.019877,0.042272&amp;hl=en&amp;ie=UTF8&amp;ll=35.226805,-75.63897&amp;spn=0.005013,0.010568&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small></center>
+<p /><center><iframe width="95%" height="500" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d32897.3821685685!2d-75.63897!3d35.226805!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89a42df5ee4d46b1%3A0xdb3caa0bb7ce1daa!2s54195%20Osprey%20Way%2C%20Frisco%2C%20NC%2027953!5e1!3m2!1sen!2sus!4v1567219630446!5m2!1sen!2sus" frameborder="0" style="border:0;" allowfullscreen=""></iframe><br /><small><a href="http://maps.google.com/maps?q=54195 Osprey Way++Frisco+NC+27936 (OBX Star Gaze)&amp;num=1&amp;t=h&amp;vpsrc=0&amp;sll=35.227041,-75.638466&amp;sspn=0.019877,0.042272&amp;hl=en&amp;ie=UTF8&amp;ll=35.226805,-75.63897&amp;spn=0.005013,0.010568&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small></center>
+	
 </div>
 </center>
 </body>
