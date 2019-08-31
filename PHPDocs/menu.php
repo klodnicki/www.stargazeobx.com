75dded24cd94a55923c7ec19eee41d286dd16eb1
Zack Klodnicki <klodzack@udel.edu>
2019-08-30 22:55:50 -0400

    Open "Rates" in a new tab

--- PHPDocs/menu.php
+++ PHPDocs/menu.php
@@ -28,7 +28,7 @@
 <a href="photos.php">Photos</a> | 
 <a href="Amenities.php">Amenities</a> | 
 <a href="map.php">Map</a> | 
-<a href="http://www.hatterasrealty.com/rental/house.html?ID=100#rate">Rates</a><!-- |
+<a href="http://www.hatterasrealty.com/rental/house.html?ID=100#rate" target="_blank">Rates</a><!-- |
 <a href="http://www.hatterasrealty.com/calendar/availcal.html?ID=100&UN=Star%2BGaze&StartDay=SUN&Arrival=&Stay=">Availability</a>--> |
 <a href="info.php">Information</a><!-- |
 <a href="weather.php">Weather</a>--><!-- |
