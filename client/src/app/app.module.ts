2b5b4263dd95edc204396485a1d6f5be77e04c17
Zack Klodnicki <klodzack@udel.edu>
2019-09-03 23:47:06 -0400

    Route animation!

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { Error404Component } from './pages/error404/error404.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- client/src/app/layout/layout.component.ts
+++ client/src/app/layout/layout.component.ts
@@ -1,9 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, query, style, animate, group, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  animations: [
+    trigger('routeAnimation', [
+      transition('* => *', [
+        group([
+          query(
+            ':leave',
+            [style({ position: 'relative', zIndex: 0 })],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [
+              style({ position: 'relative', overflow: 'visible', height: '0px', opacity: 0, zIndex: 1 }),
+              animate('0.6s linear', style({ opacity: 1 }))
+            ],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [
+              style({ transform: 'rotate(-5deg) translateX(10vw)' }),
+              animate('0.6s ease-out', style({ transform: 'translateX(0)' }))
+            ],
+            { optional: true }
+          )
+        ])
+      ])
+    ])
+  ]
 })
 export class LayoutComponent implements OnInit {
 
